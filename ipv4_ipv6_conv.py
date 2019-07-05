dc57a059c8e3b33eaab863117cfb14c3339297b6
Eric <campagnollo2002@gmail.com>
2019-07-05 17:56:05 -0400

    Update ipv4_ipv6_conv.py

--- ipv4_ipv6_conv.py
+++ ipv4_ipv6_conv.py
@@ -1,53 +1,67 @@
-import tkinter as tk
-
-
-HEIGHT=700
-WIDTH=800
+from tkinter import *
+import xml.etree.ElementTree as et
 
 def main():
+    tree=et.parse("ipv4_to_ipv6.xml")
+    xmlroot=tree.getroot()
+    subnets={}
+    for child in xmlroot.findall('subnet'):
+        subnets[child.get('ipv4')]=child[0].text
+    interface=gui(subnets)
 
-    interface=gui()
-    #interface.runner()
 
-
-def convert(v4):
-    gui.converted(v4)
 
 
 class gui():
 
-    def __init__(self):
+    def __init__(self, subnets):
+        self.subnets=subnets
+        self.root = Tk()
 
-        self.root = tk.Tk()
+        self.num1Entry = Entry(self.root)
+        self.num1Entry.pack(side=LEFT)
 
-        self.canvas = tk.Canvas(self.root,  height=HEIGHT, width=WIDTH)
-        self.canvas.pack()
+        #Label(self.root, text="+").pack(side=LEFT)
 
-        self.frame = tk.Frame(self.root, bg="#80c1ff", bd=5)
-        self.frame.place(relx=0.5, rely=0.1, relwidth=0.75, relheight=0.1, anchor='n')
+        #self.num2Entry = Entry(self.root)
+        #self.num2Entry.pack(side=LEFT)
 
-        self.entry = tk.Entry(self.frame, font=40, bg='green')
-        self.entry.place(relwidth=0.65, relheight=1)
+        self.equalButton = Button(self.root, text="convert to ipv6")
+        self.equalButton.bind("<Button-1>", self.get_sum)
+        self.equalButton.pack(side=LEFT)
 
-        self.button = tk.Button(self.frame, text="Convert", font=40, command=lambda: convert(self.entry.get()))
-        self.button.place(relx=0.7, rely=0, relwidth=0.3, relheight=1)
-
-        self.lower_frame=tk.Frame(self.root, bg='#80c1ff', bd=10)
-        self.lower_frame.place(relx=0.5, rely=0.25, relwidth=0.75, relheight=0.6, anchor='n')
-
-        self.label = tk.Label(self.lower_frame)
-        self.label.place( relwidth=1, relheight=1)
-
-        #self.label['text'] = "v4"
+        self.sumEntry = Entry(self.root, width=30)
+        self.sumEntry.pack(side=LEFT)
 
         self.root.mainloop()
 
-    def converted(self, v4):
-        self.lower_frame = tk.Frame(self.root, bg='#80c1ff', bd=10)
-        self.lower_frame.place(relx=0.5, rely=0.25, relwidth=0.75, relheight=0.6, anchor='n')
+    def get_sum(self, event):
+        ipv4=str(self.num1Entry.get())
+        self.sumEntry.delete(0,END)
 
-        self.label = tk.Label(self.lower_frame, text=str(v4))
-        self.label.place(relx=0.7, rely=0, relwidth=1, relheight=1)
+        multi=1
+        sum=0
+        netid=0
+        while ipv4[-1]!=".":
+            raw=int(ipv4[-1])
+            sum+=raw*multi
+            #sum+=raw
+            ipv4=ipv4[:-1]
+            multi*=10
+        netid=sum
+        netid6=str(hex(netid))
+        netid6=netid6[2:]
+        if len(netid6)<2:
+            netid6="0"+netid6
+        ipv4=ipv4+"xxx"
+        try:
+            ipv6=self.subnets[ipv4]
+            ipv6 = ipv6[:-2]+netid6
+        except KeyError:
+            ipv6="No known IPv4/IPv6 relationship"
+
+        self.sumEntry.insert(0, ipv6)
+
 
 
 
